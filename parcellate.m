49de13bbf04905252799528a73f2bd83f99e7098
liliumious <lily.wanqi.li@gmail.com>
2017-01-20 16:55:08 +1100

    Small tidy-ups

--- parcellate.m
+++ parcellate.m
@@ -1,18 +1,19 @@
-mripath = './MEGCanCam/MRI anatomy/sub-CC722891/anat/sub-CC722891_T1w.nii';
+outpath = './sub891/';
+load([outpath 'headmodel'], 'mri_unknown')
 
-mri_unknown = ft_read_mri(mripath)
-mri_unknown = ft_determine_coordsys(mri_unknown, 'interactive', 'yes');
+aalpath = 'ROI_MNI_V4.nii'; 
 
 cfg          = [];
 cfg.method   = 'interactive';
 cfg.coordsys = 'spm';
-mri_spm    = ft_volumerealign(cfg,mri_unknown)
+mri_spm      = ft_volumerealign(cfg,mri_unknown)
 
-% aal = ft_read_atlas(aalpath);
-% cfg = [];
-% cfg.roi = 'Rectus_L';
-% cfg.inputcoord = 'mni';
-% cfg.atlas = aal;
-% mask = ft_volumelookup(cfg, mri_spm);
-% 
+%% Trialing on left Gyrus Rectus
+aal = ft_read_atlas(aalpath);
+cfg = [];
+cfg.roi = 'Rectus_L';
+cfg.inputcoord = 'mni';
+cfg.atlas = aal;
+mask = ft_volumelookup(cfg, mri_spm);
+
 
