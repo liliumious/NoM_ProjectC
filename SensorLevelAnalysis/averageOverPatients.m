f2a778d7b4dab42d4ab57991eac6095314a4c329
Justin Phu <jphu0343@uni.sydney.edu.au>
2017-02-03 15:00:50 +1100

    SensorLevel folder creation
    Moving things into different folders with some modifications of the
    comments

--- SensorLevelAnalysis/averageOverPatients.m
+++ SensorLevelAnalysis/averageOverPatients.m
@@ -1,4 +1,6 @@
-%Averaging (timelock) over all patients
+%% Creating a grand average timelock over all patients
+
+%Directories (Remember to change everytime)
 datapath = 'C:\Users\Lily\Desktop\CamCan\';
 currentDirectory = 'C:\Users\Lily\Documents\NoM_ProjectC\';
 
@@ -7,22 +9,30 @@ currentLine = fgets(file);
 
 %We will store all our data in tlockAll
 % tlock{i} will be the data for the ith stimulus
+% i = 
+% 1 Aud Only
+% 2 AudVid 300
+% 3 AudVid 600
+% 4 AudVid 1200
+% 5 Vid Only
 tlockAll = {};
 
-%For all the stimuli
 
 j = 1;
-%For all the patients we have
+%For all the patients we have downloaded
 while ischar(currentLine)
     
-    %Return -1 if patient does not exist (not downloaded)
-    %Returns an cell array of the tlock for each stimulus
+    %Return empty struct if patient does not exist (not downloaded)
+    %Returns an cell array of the tlock & before timelock for each stimulus
     [currentFT, currentTL] = getSourceData_Function(currentLine, datapath, currentDirectory);
+    
     if(~isempty(currentData))
         %Every stimuli
         for i = 1:5
             %Store the tlock data in a cell array
             tlockAll{i,j} = currentTL{i};
+            
+            %Store the ft data
             ftAll{i,j} = currentFT{i};
         end
         j = j + 1;
