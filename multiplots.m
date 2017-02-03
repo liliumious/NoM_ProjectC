a15d1c4315a38fa53b3f9c13cf70bc5e89da030c
Lily Li <liliumious@gmail.com>
2017-02-03 14:27:56 +1100

    Plots

---
+++ multiplots.m
@@ -0,0 +1,24 @@
+%% TEMP DRAFT
+data = currentFT{2};
+
+cfg = [];
+cfg.channel= 'megplanar';	 
+cfg.method = 'wavelet';
+cfg.output = 'pow';
+cfg.foi    = 1:2:30;	                
+cfg.toi    = -0.5:0.05:1.5;	
+freq = ft_freqanalysis(cfg, data)
+
+cfg = [];    
+cfg.showlabels   = 'yes';
+cfg.layout = 'neuromag306all.lay';
+figure
+ft_multiplotTFR(cfg, freq)
+
+load([currentDirectory 'results'],'tlockAll');
+grandavg = ft_timelockgrandaverage([],tlockAll{2,:});
+cfg = [];    
+cfg.showlabels   = 'yes';
+cfg.layout = 'neuromag306all.lay';
+figure
+ft_multiplotER(cfg, grandavg)
