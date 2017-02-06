5cb93b3171d6cf67b7fad4749c8eab328978b558
Lily Li <liliumious@gmail.com>
2017-02-06 16:54:42 +1100

    Big commit

--- tempfile.m
+++ tempfile.m
@@ -153,6 +153,9 @@ cfg.lcmv.keepfilter='yes';
 cfg.channel = dataica.label;
 sourceavg=ft_sourceanalysis(cfg, avg);
 
+
+%%
+
 cfg=[];
 cfg.method='lcmv';
 cfg.grid=grid;
@@ -167,15 +170,17 @@ cfg.operation = '((x1-x2)./x2)*100';
 S1bl=ft_math(cfg,sourcepstS1,sourcepreS1);
 
 template_mri = ft_read_mri('spm8T1.nii');
+aalpath = 'ROI_MNI_V4.nii';
+aal = ft_read_atlas(aalpath);
+
 cfg              = [];
 cfg.voxelcoord   = 'no';
 cfg.parameter    = 'pow';
 cfg.interpmethod = 'nearest';
 source_int  = ft_sourceinterpolate(cfg, S1bl, template_mri);
 
-%%
 cfg=[];
-parcel = ft_sourceparcellate(cfg, source_int, atlas);
+parcel = ft_sourceparcellate(cfg, source_int, aal);
 
 dummy=atlas;
 for i=1:length(parcel.pow)
@@ -193,3 +198,114 @@ cfg.location = [-42 -20 6];
 cfg.atlas = atlas;
 cfg.funcolorlim = [-30 30];
 ft_sourceplot(cfg,source_int);
+
+
+cfg = [];
+cfg.method         = 'surface';
+cfg.funparameter   = 'parcel';
+cfg.funcolorlim    = [-30 30];
+cfg.funcolormap    = 'jet'; 
+cfg.projmethod     = 'nearest'; 
+cfg.surfinflated   = 'surface_inflated_both_caret.mat';
+cfg.projthresh     = 0.95;
+cfg.camlight       = 'no';
+ft_sourceplot(cfg, source_int);
+view ([-70 20 50])
+light ('Position',[-70 20 50])
+material dull
+
+%%
+cfg = [];
+cfg.covariance='yes';
+cfg.keeptrials = 'yes';
+avgpre = ft_timelockanalysis(cfg,datapre);
+avgpst = ft_timelockanalysis(cfg,datapost);
+
+cfg=[];
+cfg.method='lcmv';
+cfg.grid=grid;
+cfg.vol=hdm;
+cfg.lcmv.keepfilter='yes';
+cfg.channel = dataica.label;
+sourceavg=ft_sourceanalysis(cfg, avg);
+cfg=[];
+cfg.method='lcmv';
+cfg.grid=grid;
+cfg.grid.filter=sourceavg.avg.filter;
+cfg.rawtrial = 'yes';
+cfg.vol=hdm;
+sourcepreS1=ft_sourceanalysis(cfg, avgpre);
+sourcepstS1=ft_sourceanalysis(cfg, avgpst);
+
+cfg = [];
+cfg.parameter    = 'pow';
+cfg.dim          = grid.dim;
+cfg.method           = 'montecarlo';
+cfg.statistic        = 'ft_statfun_depsamplesT';
+cfg.correctm         = 'cluster';
+cfg.clusteralpha     = 0.05;
+cfg.clusterstatistic = 'maxsum';
+cfg.tail             = 0;
+cfg.clustertail      = 0;
+cfg.alpha            = 0.025;
+cfg.numrandomization = 1000;
+ 
+ntrials = numel(sourcepreS1.trial);
+design  = zeros(2,2*ntrials);
+design(1,1:ntrials) = 1;
+design(1,ntrials+1:2*ntrials) = 2;
+design(2,1:ntrials) = [1:ntrials];
+design(2,ntrials+1:2*ntrials) = [1:ntrials];
+ 
+cfg.design   = design;
+cfg.ivar     = 1;
+cfg.uvar     = 2;
+stat = ft_sourcestatistics(cfg,sourcepstS1,sourcepreS1);
+stat.pos=template_grid.pos;% keep positions for plotting later
+
+stat.inside=template_grid.inside;
+cfg              = [];
+cfg.voxelcoord   = 'no';
+cfg.parameter    = 'stat';
+cfg.interpmethod = 'nearest';
+statint  = ft_sourceinterpolate(cfg, stat, template_mri);
+cfg.parameter    = 'mask';
+maskint  = ft_sourceinterpolate(cfg, stat, template_mri);
+statint.mask = maskint.mask;
+
+
+statint.coordsys = 'mni';
+cfg               = [];
+cfg.method        = 'ortho';
+cfg.funparameter  = 'stat';
+cfg.maskparameter = 'mask';
+cfg.atlas         = atlas;
+cfg.location = 'max';
+cfg.funcolorlim   = [-5 5];
+cfg.funcolormap = 'jet';
+ft_sourceplot(cfg,statint);
+
+%%
+cfg=[];
+parcel = ft_sourceparcellate(cfg, statint, aal);
+parcelmask = ft_sourceparcellate(cfg, maskint, aal);
+%% create dummy struct
+dummy=aal;
+dummymask = aal;
+for i=1:length(parcel.stat)
+      dummy.tissue(find(dummy.tissue==i))=parcel.stat(i);
+      dummymask.tissue(find(dummymask.tissue==i))=parcelmask.mask(i);
+end;
+%% plot the result
+statint.parcel=dummy.tissue;
+statint.coordsys = 'mni';
+statint.mask  = dummymask.tissue;
+cfg=[];
+cfg.method = 'slice';
+cfg.funparameter = 'parcel';
+cfg.funcolormap    = 'jet';
+cfg.maskparameter = 'mask';
+cfg.renderer = 'zbuffer';
+cfg.funcolorlim   = [-5 5];
+cfg.atlas = atlas;
+ft_sourceplot(cfg,statint);
